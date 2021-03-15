e1a57c4afc74eeea4add61bee3491d4cd8530fb4
David Fowler <dafowler17@gmail.com>
2021-03-14 22:37:49 -0400

    Add name

--- hello-world.cpp
+++ hello-world.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+using namespace std;
 int main() 
 {
-  std::cout << "Hello World." << std::endl;
+//std::cout << "Hello World." << std::endl;
+  string name;
+  cout<<"What is your name? ";
+  cin>>name;
+  cout<< "Hello, "<< name<< ".\n";
   return 0;
 }
